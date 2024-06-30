b68135b29312b72ad1b66da9ab15a64852b82dbc
ro <15965394977@163.com>
2024-06-30 22:38:48 +0800

    fix bug: suffix with gif

--- crawl.py
+++ crawl.py
@@ -21,10 +21,14 @@ def webtoon(id,hua):
     r = requests.get(url,headers=headers)
     soup=bs(r.text,'html.parser')
     wt = soup.find('div',class_='wt_viewer')
+    # print(wt)
     imgs= wt.find_all('img',recursive=False)
     for i,img in enumerate(imgs):
         img_url=img['src']
-        if not img_url.endswith("jpg"):
+        # some webtoon suffix is gif, dnot know why
+        # if not img_url.endswith("jpg"):
+        #     continue
+        if img_url.find(str(id)) <0 :
             continue
         print(img_url)
         r=requests.get(img_url,headers=headers)
@@ -32,8 +36,8 @@ def webtoon(id,hua):
         img_name=f'img_{i}.jpg'
         with open(img_name,'wb') as f:
             f.write(r.content)
-        i=Image.open(img_name)
-        w,h=i.size
-        if w>h:
-            i.close()
-            os.remove(img_name)
+        # i=Image.open(img_name)
+        # w,h=i.size
+        # if w>h:
+        #     i.close()
+        #     os.remove(img_name)
--- zipit.py
+++ zipit.py
@@ -3,23 +3,41 @@ from glob import glob
 import os
 from crawl import webtoon
 xinhun=812354
+samguk=711422
+holangi=650305
+pandag=61731
 id=812356
-id=xinhun
+id=pandag
 name=1
 with open('number','r') as num:
     name = int(num.readline())
-hua=name
-webtoon(id,hua)
 
-imgs = glob("*.jpg")
-zip_name=f'{name}.zip'
-print(zip_name)
-with ZipFile(zip_name,'w')  as pszip:
+def img_zip(imgs,name):
+    suffix='cbz'
+    zip_name=f'{name}.{suffix}'
+    print(zip_name)
+    with ZipFile(zip_name,'w')  as pszip:
+        for img in imgs:
+            pszip.write(img)
+
+hua_len = 2
+dir='pandag'
+if not os.path.exists(dir):
+    os.makedirs(dir)
+for i in range(hua_len):
+    print(f'{i}/{hua_len}')
+    imgs=[]
+    cnt=0
+    while len(imgs)<=0:
+        if cnt>1:
+            print(f'{name} hua not get, try again,{cnt}')
+        webtoon(id,name)
+        imgs = glob("*.jpg")
+        cnt+=1
+    zip_name = os.path.join(dir,str(name))
+    img_zip(imgs,zip_name)
+    name+=1
     for img in imgs:
-        pszip.write(img)
-
-for img in imgs:
-    os.remove(img)
-
-with open('number','w') as num:
-    num.write(f'{name+1}')
+        os.remove(img)
+    with open('number','w') as num:
+        num.write(f'{name}')
